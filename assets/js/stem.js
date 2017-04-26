8646d13079e54836b036ae8c65de15810633c385
Liam Ellis <liamhellis@gmail.com>
2017-04-25 20:09:59 -0400

    stuff

--- assets/js/stem.js
+++ assets/js/stem.js
@@ -1,145 +1,53 @@
-(function($) {
-
-	$(document).ready(function() {
-		setupFade();
-		setupClickToScroll();
-		setupPostAnimation();
-		setupScrollToTop();
-     enableScrollAbortion();
-
-		// Trigger window.scroll, this will initiate some of the scripts
-		$(window).scroll();
-  });
-
-
-  // Allow user to cancel scroll animation by manually scrolling
-  function enableScrollAbortion() {
-    var $viewport = $('html, body');
-    $viewport.on('scroll mousedown DOMMouseScroll mousewheel keyup', function(e) {
-        if ( e.which > 0 || e.type === 'mousedown' || e.type === 'mousewheel') {
-             $viewport.stop();
-        }
-    });
-  }
-
-	function setupScrollToTop() {
-		var scrollSpeed = 750;
-		$('.trigger-scroll-to-top').click(function(e) {
-			e.preventDefault();
-			$('html, body').animate({
-				scrollTop: 0
-			}, scrollSpeed);
-		});
-	}
-
-
-	function setupPostAnimation() {
-		var posts = $('.post-wrapper .post');
-		$(window).on('scroll resize', function() {
-
-			var currScroll = $(window).scrollTop() > $(document).scrollTop() ? $(window).scrollTop() : $(document).scrollTop(),
-				windowHeight = $(window).height(), // Needs to be here because window can resize
-				overScroll = Math.ceil(windowHeight*.20),
-				treshhold = (currScroll + windowHeight) - overScroll;
-
-			posts.each(function() {
-
-				var post = $(this),
-					postScroll = post.offset().top
-
-				if(postScroll > treshhold) {
-					post.addClass('hidden');
-				} else {
-					post.removeClass('hidden');
-				}
-
-			});
-
-		});
-	}
-
-	function setupFade() {
-
-		var posts = $('.post-wrapper .post').reverse(),
-			stemWrapper = $('.stem-wrapper'),
-			halfScreen = $(window).height() / 2;
-
-		$(window).on('scroll resize', function() {
-
-			delay(function() {
-
-				var currScroll = $(window).scrollTop() > $(document).scrollTop() ? $(window).scrollTop() : $(document).scrollTop(),
-					scrollSplit = currScroll + halfScreen;
-
-				posts.removeClass('active').each(function() {
-
-					var post = $(this),
-						postOffset = post.offset().top;
-
-					if(scrollSplit > postOffset) {
-
-						// Add active class to fade in
-						post.addClass('active')
-
-						// Get post color
-						var color = post.data('stem-color') ? post.data('stem-color') : null,
-							allColors = 'color-green color-yellow color-white'
-
-						stemWrapper.removeClass(allColors);
-
-						if(color !== null) {
-							stemWrapper.addClass('color-' + color);
-						}
-
-						return false;
-					}
-
-				});
-			}, 20);
-
-		});
-
-	}
-
-
-	function setupClickToScroll(post) {
-
-		var scrollSpeed = 750;
-
-		$('.post-wrapper .post .stem-overlay .icon').click(function(e) {
-			e.preventDefault();
-
-			var icon = $(this),
-				post = icon.closest('.post'),
-				postTopOffset = post.offset().top,
-				postHeight = post.height(),
-				halfScreen = $(window).height() / 2,
-				scrollTo = postTopOffset - halfScreen + (postHeight/2);
-
-			$('html, body').animate({
-				scrollTop: scrollTo
-			}, scrollSpeed);
-		});
-
-	}
-
-})(jQuery);
+$(function(){
+	var controller = new ScrollMagic.Controller();
+	var timeLine = $('#timeline');
+	var ion = $('.ion-container');
+	console.log($('#scroll-body').height());
+	console.log(controller);
+	console.log(timeLine);
+	TweenMax.set(timeLine, {height: '0'});
 
 
 
+	var tweenOne = TweenMax.fromTo(timeLine, 5, {height: '0%'}, {height: '20%'});
 
-/*==========  Helpers  ==========*/
+	var scene1 = new ScrollMagic.Scene({
+		offset: 300,
+	  triggerElement: '#scroll-body',
+	}).setTween(tweenOne).addTo(controller);
+
+	var ionTween = TweenMax.to(ion, 5, {borderRadius: '50%', border: '2px solid #333'});
+
+	var sceneIon = new ScrollMagic.Scene({
+
+	  triggerElement: '#post-2',
+	}).setTween(sceneIon).addTo(controller);
 
 
-// Timeout function
-var delay = (function(){
-	var timer = 0;
-	return function(callback, ms){
-		clearTimeout (timer);
-		timer = setTimeout(callback, ms);
-	};
-})();
+	var tweenTwo = TweenMax.fromTo(timeLine, 2,{height:"20%"}, {height:"40%"});
 
-$.fn.reverse = function() {
-    return this.pushStack(this.get().reverse(), arguments);
-};
+	var scene2 = new ScrollMagic.Scene({
+	  triggerElement: '#post-2',
+	}).setTween(tweenTwo).addTo(controller);
+
+
+	var tweenThree = TweenMax.fromTo(timeLine, 2,{height:"40%"}, {height:"60%"});
+
+	var scene3 = new ScrollMagic.Scene({
+	  triggerElement: '#post-3',
+	}).setTween(tweenThree).addTo(controller);
+
+
+	var tweenFour = TweenMax.fromTo(timeLine, 2,{height:"60%"}, {height:"80%"});
+
+	var scene4 = new ScrollMagic.Scene({
+	  triggerElement: '#post-4',
+	}).setTween(tweenFour).addTo(controller);
+
+	var tweenFive = TweenMax.fromTo(timeLine, 2,{height:"80%"}, {height:"150%"});
+
+	var scene4 = new ScrollMagic.Scene({
+	  triggerElement: '#post-5',
+  }).setTween(tweenFive).addTo(controller);
+
+});
